1526ae6464fb7318bb31918992cc11ccf8001138
Gabriel-D-Silva <gabriel.d.silva063@gmail.com>
2024-09-19 07:57:14 -0300

    Fiz um front e back end simples para tentar fazer a automação funcionar de forma mais amigavel pro usuario, porém está dando algum erro estranho onde a automação não puxa os links dos produtos e retorna um objeto vazio, estou commitando nesse ponto somente pois quero um backup antes de resolver esse problema

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const scrapeMercadoLivre = require('./scraping')
+const cors = require('cors')
+
+const porta = 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/scrape', async (req, res) => {
+
+  const produto = req.query.produto;
+
+  try {
+
+    // Aguarda o resultado da função de scraping
+    const resultado = await scrapeMercadoLivre(produto);
+
+    // Exibe o resultado no console
+    console.log(resultado);
+
+    // Envia o resultado como resposta
+    res.json(resultado);
+  
+  } catch (error) {
+
+    console.error('Erro na rota /scrape:', error.message);
+    res.status(500).send('Erro no servidor');
+
+  }
+})
+
+// Inicia o servidor na porta 5000 e imprime uma mensagem para indicar que está rodando
+app.listen(porta, () => {
+    console.log(`Server rodando na porta ${porta}`)
+});
